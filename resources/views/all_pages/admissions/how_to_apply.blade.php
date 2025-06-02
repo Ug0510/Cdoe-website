a9c199d1067e9cae7a449cd698d71a84253ae1e3
Akash <akashk.webdev@tmu.ac.in>
2025-06-02 12:47:03 +0530

    update how to apply page

--- resources/views/all_pages/admissions/how_to_apply.blade.php
+++ resources/views/all_pages/admissions/how_to_apply.blade.php
@@ -25,35 +25,12 @@
             </div>
         </div> -->
         <div class="course-course-detaila-inner">
-            <div class="course-details-nav-tab text-center">
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab"
-                            aria-controls="tab1" aria-selected="true">Online Mode</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2"
-                            aria-selected="false">Offline Mode</a>
-                    </li>
-                </ul>
-            </div>
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
                     <div class="ratings-list-inner mb-4">
                         <div class="course-details-content text-center">
                             <h4 class="title">For Online Application, follow the steps below</h4>
                             <img src="{{ asset('assets/img/admission2.png') }}" alt="img" class="img-fluid mb-4"
-                                style="width: 100%; height: auto;">
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
-                    <div class="ratings-list-inner mb-4">
-                        <div class="course-details-content text-center">
-                            <h4 class="title">For Online Application follow the Steps below</h4>
-                            <img src="{{ asset('assets/img/admission1.webp') }}" alt="img" class="img-fluid mb-4"
                                 style="width: 100%; height: auto;">
                         </div>
                     </div>
